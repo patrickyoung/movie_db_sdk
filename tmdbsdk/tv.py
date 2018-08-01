37b964995972ed6f70480efb0cff063975326efe
Patrick Young <younc25@labcorp.com>
2018-07-31 23:42:51 -0400

    return top 100 most popular tv shows

--- tmdbsdk/tv.py
+++ tmdbsdk/tv.py
@@ -1,16 +1,22 @@
 from . import api_session
 
+TMDB_API_URL = 'https://api.themoviedb.org/3'
+ITEMS_PER_PAGE = 20
+
 class TV(object):
   def __init__(self, id):
     self.id = id
   
   def info(self):
-    path = 'https://api.themoviedb.org/3/tv/{}'.format(self.id)
+    path = '{}/tv/{}'.format(TMDB_API_URL, self.id)
     response = api_session.get(path)
     return response.json()
 
   @staticmethod
   def popular():
-    path = "https://api.themoviedb.org/3/tv/popular"
-    response = api_session.get(path)
-    return response.json()
+    top_num = (100 // ITEMS_PER_PAGE) + 1
+    response = []
+    for page in range(1, top_num):
+      path = '{}/tv/popular?page={}'.format(TMDB_API_URL, page)
+      response += api_session.get(path).json()['results']
+    return response
